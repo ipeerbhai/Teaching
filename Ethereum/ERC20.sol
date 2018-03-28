b6750027d29b2b51fbd7291d2f5d3df32e738a14
Imran Peerbhai <ipeerbhai@aol.com>
2018-03-28 15:28:15 -0700

    Adding readme, a single contract, and a gitignore to prevent checking in build objects.

---
+++ Ethereum/ERC20.sol
@@ -0,0 +1,95 @@
+pragma solidity ^0.4.18; 
+
+
+// copied from official github, changed to interface from contract
+// dropped member variables, as 4.16+ doesn't allow these any longer in interfaces.
+interface ERC20 {
+    function totalSupply() public constant returns (uint _totalSupply);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferfrom(address fro, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract ThinkSampleToken is ERC20 {
+
+    string public constant symbol = "TT";
+    string public constant name = "ThinkPredict ERC20 Example Token";
+    uint8 public constant decimals = 18; // Which format is our token matching ( wei, in this case )
+
+    address private owner;
+    uint private constant tokenTotal = 100; // we'll only ever mint 100 of these...
+    uint minted;
+    mapping(address => uint) private ownerBalance;
+    mapping(address => mapping(address=>uint)) private allowedToSpend;
+
+    
+
+    function totalSupply() public constant returns (uint _totalSupply) {
+        _totalSupply = tokenTotal;
+        return _totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public constant returns (uint balance) {
+        balance = ownerBalance[tokenOwner];
+    }
+
+
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining) {
+        // TODO: .18+ removes allowing a simple if ( address ) boolean.  Need to find blessed function.
+        // for now, just return assuming a valid tokenowner.
+
+            remaining = allowedToSpend[tokenOwner][spender];
+            return remaining;
+    }
+    
+    
+    function transfer(address to, uint tokens) public returns (bool success) {
+        success = false;
+            if (tokens <= balanceOf(msg.sender)) {
+                ownerBalance[msg.sender] -= tokens;
+                ownerBalance[to] += tokens;
+                success = true;
+                Transfer(msg.sender, to, tokens);
+            }
+        return success;
+    }
+
+    function approve(address spender, uint tokens) public returns (bool success) {
+        success = false;
+        if (tokens <= balanceOf(msg.sender)) {
+            allowedToSpend[msg.sender][spender] = tokens;
+            success = true;
+            Approval(msg.sender, spender, tokens);
+        }
+        return success;
+
+    }
+
+    function transferfrom(address from, address to, uint tokens) public returns (bool success) {
+        success = false;
+        if ( (allowance(from, msg.sender) >= tokens) && (tokens >= 0) ) {
+            ownerBalance[from] -= tokens;
+            ownerBalance[to] += tokens;
+            allowedToSpend[from][msg.sender] -= tokens;
+            success = true;
+            Transfer(from, to, tokens);
+        }
+        return success;
+    }
+
+    function mint() public {
+        if ((ownerBalance[msg.sender] < 1) && (minted < tokenTotal)) { 
+            ownerBalance[msg.sender] += 1;
+            minted++;
+        }
+    }
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-Hello
+# Class Notes #
+This repo is a set of experimental programs I've created to help students of DApp development get a better sense of what some of these items mean.
+
+## Ethereum ##
+This directory contains simple examples of smart contracts.
