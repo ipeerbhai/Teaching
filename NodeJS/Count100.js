5d543a6395dcff0ce827b39ccf41a715baf24a16
Imran Peerbhai <ipeerbhai@aol.com>
2018-05-09 14:33:38 -0700

    adding 0..100 counting with output.

---
+++ NodeJS/Count100.js
@@ -0,0 +1,9 @@
+for (let i = 0; i <= 100; i++) { // count 0 .. 100
+    if (i < 100) {
+        process.stdout.write(i + ", "); // output the number with  a comma.
+    } else {
+        console.log(i); //output numbers greater than 99 without a trailing ","
+    }
+
+    // There's another way to accomplish this goal that isn't dependent on an if statement using strings.  Try and figure that out.
+}
